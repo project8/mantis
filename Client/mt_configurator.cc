dac35dec63685a6fcf9a19cb245ef333d9f66d96
Noah Oblath <nsoblath@mit.edu>
2013-11-07 17:39:57 -0500

    Added configuration value access to configurator.

--- Client/mt_configurator.cc
+++ Client/mt_configurator.cc
@@ -12,7 +12,6 @@
 #include "filestream.h"
 
 #include <cstdio>
-#include <string>
 using std::string;
 
 #include <iostream>
@@ -21,7 +20,8 @@ namespace mantis
 {
 
     configurator::configurator( int an_argc, char** an_argv, configuration* a_default ) :
-            f_master_config()
+            f_master_config(),
+            f_string_buffer()
     {
         parser t_parser( an_argc, an_argv );
         std::cout << "options parsed" << std::endl;
@@ -114,6 +114,92 @@ namespace mantis
     {
         return f_master_config;
     }
+
+    bool configurator::get_bool_required( const std::string& a_name )
+    {
+        if( f_master_config.HasMember( a_name.c_str() ) )
+        {
+            return f_master_config[ a_name.c_str() ].GetBool();
+        }
+        throw exception() << "configurator does not have a value for <" << a_name << ">";
+    }
+    bool configurator::get_bool_optional( const std::string& a_name, bool a_default )
+    {
+        if( f_master_config.HasMember( a_name.c_str() ) )
+        {
+            return f_master_config[ a_name.c_str() ].GetBool();
+        }
+        return a_default;
+    }
+
+    int configurator::get_int_required( const std::string& a_name )
+    {
+        if( f_master_config.HasMember( a_name.c_str() ) )
+        {
+            return f_master_config[ a_name.c_str() ].GetInt();
+        }
+        throw exception() << "configurator does not have a value for <" << a_name << ">";
+    }
+    int configurator::get_int_optional( const std::string& a_name, int a_default )
+    {
+        if( f_master_config.HasMember( a_name.c_str() ) )
+        {
+            return f_master_config[ a_name.c_str() ].GetInt();
+        }
+        return a_default;
+    }
+
+    unsigned configurator::get_uint_required( const std::string& a_name )
+    {
+        if( f_master_config.HasMember( a_name.c_str() ) )
+        {
+            return f_master_config[ a_name.c_str() ].GetUint();
+        }
+        throw exception() << "configurator does not have a value for <" << a_name << ">";
+    }
+    unsigned configurator::get_uint_optional( const std::string& a_name, unsigned a_default )
+    {
+        if( f_master_config.HasMember( a_name.c_str() ) )
+        {
+            return f_master_config[ a_name.c_str() ].GetUint();
+        }
+        return a_default;
+    }
+
+    double configurator::get_double_required( const std::string& a_name )
+    {
+        if( f_master_config.HasMember( a_name.c_str() ) )
+        {
+            return f_master_config[ a_name.c_str() ].GetDouble();
+        }
+        throw exception() << "configurator does not have a value for <" << a_name << ">";
+    }
+    double configurator::get_double_optional( const std::string& a_name, double a_default )
+    {
+        if( f_master_config.HasMember( a_name.c_str() ) )
+        {
+            return f_master_config[ a_name.c_str() ].GetDouble();
+        }
+        return a_default;
+    }
+
+    const std::string& configurator::get_string_required( const std::string& a_name )
+    {
+        if( f_master_config.HasMember( a_name.c_str() ) )
+        {
+            return f_string_buffer.assign( f_master_config[ a_name.c_str() ].GetString() );
+        }
+        throw exception() << "configurator does not have a value for <" << a_name << ">";
+    }
+    const std::string& configurator::get_string_optional( const std::string& a_name, const std::string& a_default )
+    {
+        if( f_master_config.HasMember( a_name.c_str() ) )
+        {
+            return f_string_buffer.assign( f_master_config[ a_name.c_str() ].GetString() );
+        }
+        return f_string_buffer.assign( a_default );
+    }
+
 
 
 } /* namespace mantis */
