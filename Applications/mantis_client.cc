d4b68ebd9d10cc2d6dceffa7bb98d4c1c5bf3a74
Noah Oblath <nsoblath@mit.edu>
2013-11-07 14:39:06 -0500

    Fixing a stupid mistake

--- Applications/mantis_client.cc
+++ Applications/mantis_client.cc
@@ -62,6 +62,7 @@ int main( int argc, char** argv )
 
     while( true )
     {
+        t_run_context->pull_status();
       //if( ! t_run_context->pull_status() )
       //{
       //    cerr << "[mantis_client] error pulling status; quitting" << endl;
--- Server/mt_receiver.cc
+++ Server/mt_receiver.cc
@@ -30,7 +30,7 @@ namespace mantis
             t_run_context = new run_context();
             cout << "[receiver] waiting for incoming connections" << endl;
             // thread is blocked by the accept call in server::get_connection 
-	    // until an incoming connection is received
+            // until an incoming connection is received
             t_run_context->set_connection( f_server->get_connection() );
 
             cout << "[receiver] receiving request..." << endl;
@@ -43,10 +43,11 @@ namespace mantis
                 cout << "pushing status; result: " << t_run_context->push_status() << endl;
                 // wait a second to make sure status is received before deleting context
                 //sleep( 1 );
+                delete t_run_context->get_connection();
                 delete t_run_context;
             }
             else
-	    {
+            {
                 cout << "[receiver] sending status <acknowledged>..." << endl;
 
                 t_run_context->get_status()->set_state( status_state_t_acknowledged );
@@ -61,10 +62,10 @@ namespace mantis
                 // if the queue condition is waiting, release it
                 if( f_condition->is_waiting() == true )
                 {
-	            cout << "[receiver] releasing queue condition" << endl;
+                    cout << "[receiver] releasing queue condition" << endl;
                     f_condition->release();
                 }
-	    }
+            }
 
             cout << "[receiver] finished processing request" << endl;
         }
