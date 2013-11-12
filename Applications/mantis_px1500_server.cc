78bb38834df0ccd9fde05488664c937df8d0e869
DJSecretSnaxxx <dj_secret_snacks@hyperion-targeting-instustries.com>
2013-11-12 05:32:02 -0800

    changed uints to ints in the configuration

--- Applications/mantis_px1500_server.cc
+++ Applications/mantis_px1500_server.cc
@@ -39,10 +39,10 @@ int main( int argc, char** argv )
 
     cout << "[mantis_server] creating objects..." << endl;
 
-    server t_server( t_config.get_uint_required( "port" ) );
+    server t_server( t_config.get_int_required( "port" ) );
 
     condition t_buffer_condition;
-    buffer t_buffer( t_config.get_uint_required( "buffer-size" ), t_config.get_uint_required( "record-size" ) );
+    buffer t_buffer( t_config.get_int_required( "buffer-size" ), t_config.get_int_required( "record-size" ) );
 
     digitizer_px1500 t_digitizer( &t_buffer, &t_buffer_condition );
     writer t_writer( &t_buffer, &t_buffer_condition );
