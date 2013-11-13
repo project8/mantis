c5aa95d6550b1406dbd9be34e649d5f58f1e2faa
DJSecretSnaxxx <dj_secret_snacks@hyperion-targeting-instustries.com>
2013-11-13 02:08:01 -0800

    Improved documentation

--- Applications/mantis_server.cc
+++ Applications/mantis_server.cc
@@ -1,16 +1,29 @@
 /*
- * mantis_px1500_server.cc
+ * mantis_server.cc
  *
  *      Author: Dan Furse
  *
- *  Server component of the DAQ for the px1500 digitizer.
+ *  Server component of the DAQ for the any of the available digitizers.
+ *  Default configuration options are given in class server_config (see mt_server_config.cc)
+ *
+ *  The server can be configured in three ways, each of which can override previous values:
+ *   1. Default configuration (see class server_config in mt_server_config.cc)
+ *   2. Configuration file supplied by the user (json-formatted)
+ *   3. Command line options
+ *
+ *  The server requires the following configuration values:
+ *   - port (integer; must match the port used by the client)
+ *   - buffer-size (integer; number of record blocks in the DMA buffer)
+ *   - record-size (integer; number of samples in a record)
  *
  *  Usage:
- *  $> mantis_px1500_server port=<some port number>
+ *  $> mantis_server config=config_file.json [further configuration]
  *
  *  Arguments:
- *  - port (integer; required): port number to be opened by the server
- *
+ *  - config (string; optional): json-formatted configuration file
+ *  - further configuration: override or add new values
+ *    format: name/type=value
+ *    e.g.:   port/i=8235
  */
 
 #include "mt_exception.hh"
@@ -23,7 +36,7 @@
 #include "mt_buffer.hh"
 #include "mt_receiver.hh"
 #include "mt_worker.hh"
-#include "mt_digitizer_px1500.hh"
+#include "mt_digitizer.hh"
 #include "mt_writer.hh"
 #include "mt_thread.hh"
 using namespace mantis;
