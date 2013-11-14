7a00611b1cf4d1afe4248d420b59f045bc99e1ca
Noah Oblath <nsoblath@mit.edu>
2013-11-14 11:46:48 -0500

    Filled in pull and push functions. Added block (de)serialization.

--- Client/mt_record_dist.cc
+++ Client/mt_record_dist.cc
@@ -5,12 +5,11 @@
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
+#include <cstring> // for memcpy
 
 using std::cerr;
 using std::cout;
 using std::endl;
-
-#include <google/protobuf/text_format.h>
 
 namespace mantis
 {
@@ -20,13 +19,16 @@ namespace mantis
                     f_acquisition_id_offset( 0 ),
                     f_record_id_offset( 0 ),
                     f_timestamp_offset( 0 ),
+                    f_data_size_offset( 0 ),
                     f_data_offset( 0 )
     {
+        // f_state_offset = 0 (already set)
         f_acquisition_id_offset = f_state_offset + sizeof( block::state_type );
         f_record_id_offset = f_acquisition_id_offset + sizeof( acquisition_id_type );
         f_timestamp_offset = f_record_id_offset + sizeof( record_id_type );
-        f_data_offset = f_timestamp_offset + sizeof( time_nsec_type );
-        cout << "[record dist]: offsets: " << f_acquisition_id_offset << "  " << f_record_id_offset << "  " << f_timestamp_offset << "  " << f_data_offset << endl;
+        f_data_size_offset = f_timestamp_offset + sizeof( time_nsec_type );
+        f_data_offset = f_data_size_offset + sizeof( size_t );
+        cout << "[record dist]: offsets: " << f_acquisition_id_offset << "  " << f_record_id_offset << "  " << f_timestamp_offset << "  " << f_data_size_offset << "  " << f_data_offset << endl;
     }
     record_dist::~record_dist()
     {
@@ -52,6 +54,11 @@ namespace mantis
         cout << "[record dist] rec dist: " << (time_nsec_type)(f_buffer + f_timestamp_offset) << endl;
         return (time_nsec_type)(f_buffer + f_timestamp_offset);
     }
+    size_t record_dist::get_data_size() const
+    {
+        cout << "[record dist] data size: " << (size_t)(f_buffer + f_data_size_offset) << endl;
+        return (size_t)(f_buffer + f_data_size_offset);
+    }
     const data_type* record_dist::get_data() const
     {
         cout << "[record dist] data[0]: " << (data_type*)(f_buffer + f_data_offset)[0] << endl;
@@ -59,11 +66,67 @@ namespace mantis
     }
     bool record_dist::push_record( const block* a_block )
     {
-
+        size_t t_block_size = reset_buffer( block_size( a_block ) );
+        if( ! serialize_block( a_block ) )
+            return false;
+        try
+        {
+            f_connection->send( f_buffer, t_block_size );
+        }
+        catch( exception& e )
+        {
+            cerr << "a write error occurred while pushing a record: " << e.what() << endl;
+            return false;
+        }
+        return true;
     }
-    bool record_dist::pull_record()
+    bool record_dist::pull_record( block* a_block )
     {
-
+        size_t t_block_size = f_buffer_size;
+        try
+        {
+            t_block_size = f_connection->recv_size();
+            if( t_block_size == 0 ) return false;
+            reset_buffer( t_block_size );
+            if( f_connection->recv( f_buffer, t_block_size ) == 0 )
+                return false;
+        }
+        catch( exception& e )
+        {
+            cerr << "a read error occurred while pulling a record: " << e.what() << endl;
+            return false;
+        }
+        return deserialize_block( a_block );
     }
 
+    size_t record_dist::block_size( const block* a_block ) const
+    {
+        return sizeof( block::state_type ) +
+                sizeof( acquisition_id_type ) +
+                sizeof( record_id_type ) +
+                sizeof( time_nsec_type ) +
+                sizeof( size_t ) +
+                a_block->get_data_size();
+    }
+
+    bool record_dist::serialize_block( const block* a_block )
+    {
+        ( block::state_type )(f_buffer + f_state_offset ) = a_block->get_state();
+        ( acquisition_id_type )(f_buffer + f_acquisition_id_offset ) = a_block->get_acquisition_id();
+        ( record_id_type )(f_buffer + f_record_id_offset ) = a_block->get_record_id();
+        ( time_nsec_type )(f_buffer + f_timestamp_offset ) = a_block->get_timestamp();
+        ( size_t )(f_buffer + f_data_size_offset ) = a_block->get_data_size();
+        ::memcpy( f_buffer + f_data_offset, a_block->data(), a_block->get_data_size() );
+        return true;
+    }
+
+    bool record_dist::deserialize_block( block* a_block )
+    {
+        a_block->set_state( ( block::state_type )(f_buffer + f_state_offset ) );
+        a_block->set_acquisition_id( ( acquisition_id_type )(f_buffer + f_acquisition_id_offset ) );
+        a_block->set_record_id( ( record_id_type )(f_buffer + f_record_id_offset ) );
+        a_block->set_timestamp( ( time_nsec_type )(f_buffer + f_timestamp_offset ) );
+        a_block->set_data_size( ( size_t )(f_buffer + f_data_size_offset ) );
+        ::memcpy( a_block->data(), f_buffer + f_data_offset, a_block->get_data_size() );
+    }
 }
--- Client/mt_record_dist.hh
+++ Client/mt_record_dist.hh
@@ -18,16 +18,22 @@ namespace mantis
             acquisition_id_type get_acquisition_id() const;
             record_id_type get_record_id() const;
             time_nsec_type get_timestamp() const;
+            size_t get_data_size() const;
             const data_type* get_data() const;
             bool push_record( const block* a_block );
-            bool pull_record();
+            bool pull_record( block* a_block );
 
 
         private:
+            size_t block_size( const block* a_block ) const;
+            bool serialize_block( const block* a_block );
+            bool deserialize_block( block* a_block );
+
             unsigned f_state_offset;
             unsigned f_acquisition_id_offset;
             unsigned f_record_id_offset;
             unsigned f_timestamp_offset;
+            unsigned f_data_size_offset;
             unsigned f_data_offset;
     };
 
