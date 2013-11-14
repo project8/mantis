c7b6841e0003a0e945420013a006cbe71c025ae5
Noah Oblath <nsoblath@mit.edu>
2013-11-14 16:05:21 -0500

    Partial work on record_receiver

--- Client/mt_record_receiver.cc
+++ Client/mt_record_receiver.cc
@@ -1,4 +1,7 @@
-#include "mt_receiver.hh"
+#include "mt_record_receiver.hh"
+
+#include "mt_iterator.hh"
+#include "mt_record_dist.hh"
 
 #include <cstddef>
 
@@ -10,46 +13,76 @@ using std::endl;
 namespace mantis
 {
 
-    receiver::receiver( server* a_server, request_queue* a_request_queue, condition* a_condition ) :
+    record_receiver::record_receiver( server* a_server, buffer* a_buffer, condition* a_condition ) :
             f_server( a_server ),
-            f_request_queue( a_request_queue ),
+            f_buffer( a_buffer ),
             f_condition( a_condition )
     {
+        cout << "[record_receiver] allocating buffer..." << endl;
+
+        iterator t_it( f_buffer );
+        for( unsigned int index = 0; index < f_buffer->size(); index++ )
+        {
+            t_it->data() = new data_type[ f_buffer->record_size() ];
+            t_it->set_data_size( f_buffer->record_size() );
+            ++t_it;
+        }
     }
 
-    receiver::~receiver()
+    record_receiver::~record_receiver()
     {
+        cout << "[record_receiver] deallocating buffer..." << endl;
+
+        iterator t_it( f_buffer );
+        for( unsigned int index = 0; index < f_buffer->size(); index++ )
+        {
+            delete [] t_it->data();
+            ++t_it;
+        }
     }
 
-    void receiver::execute()
+    void record_receiver::execute()
     {
-        request_dist* t_request_dist;
+        record_dist* t_record_dist = new record_dist();
+        cout << "[record_receiver] waiting for incomming record connection" << endl;
+        // thread is blocked by the accept call in server::get_connection
+        // until an incoming connection is received
+        record_dist->set_connection( f_server->get_connection() );
+
+        iterator t_it( f_buffer );
+        while( t_record_dist->pull_record( t_it.object() ) )
+        {
+
+        }
+
+
+
 
         while( true )
         {
             t_request_dist = new request_dist();
-            cout << "[receiver] waiting for incoming connections" << endl;
+            cout << "[record_receiver] waiting for incoming connections" << endl;
             // thread is blocked by the accept call in server::get_connection 
             // until an incoming connection is received
             t_request_dist->set_connection( f_server->get_connection() );
 
-            cout << "[receiver] receiving request..." << endl;
+            cout << "[record_receiver] receiving request..." << endl;
 
             if( ! t_request_dist->pull_request() )
             {
-                cerr << "[receiver] unable to pull run request; sending status <error>" << endl;
+                cerr << "[record_receiver] unable to pull run request; sending status <error>" << endl;
                 t_request_dist->get_status()->set_state( status_state_t_error );
                 delete t_request_dist->get_connection();
                 delete t_request_dist;
             }
             else
             {
-                cout << "[receiver] sending status <acknowledged>..." << endl;
+                cout << "[record_receiver] sending status <acknowledged>..." << endl;
 
                 t_request_dist->get_status()->set_state( status_state_t_acknowledged );
                 t_request_dist->push_status();
 
-                cout << "[receiver] queuing request..." << endl;
+                cout << "[record_receiver] queuing request..." << endl;
 
                 t_request_dist->get_status()->set_state( status_state_t_waiting );
                 f_request_queue->to_back( t_request_dist );
@@ -58,12 +91,12 @@ namespace mantis
                 // if the queue condition is waiting, release it
                 if( f_condition->is_waiting() == true )
                 {
-                    cout << "[receiver] releasing queue condition" << endl;
+                    cout << "[record_receiver] releasing queue condition" << endl;
                     f_condition->release();
                 }
             }
 
-            cout << "[receiver] finished processing request" << endl;
+            cout << "[record_receiver] finished processing request" << endl;
         }
 
         return;
--- Client/mt_record_receiver.hh
+++ Client/mt_record_receiver.hh
@@ -1,27 +1,27 @@
-#ifndef MT_RECEIVER_HH_
-#define MT_RECEIVER_HH_
+#ifndef MT_RECORD_RECEIVER_HH_
+#define MT_RECORD_RECEIVER_HH_
 
 #include "mt_callable.hh"
 
 #include "mt_server.hh"
-#include "mt_request_queue.hh"
+#include "mt_buffer.hh"
 #include "mt_condition.hh"
 
 namespace mantis
 {
 
-    class receiver :
+    class record_receiver :
         public callable
     {
         public:
-            receiver( server* a_server, request_queue* a_request_queue, condition* a_condition );
-            virtual ~receiver();
+            record_receiver( server* a_server, buffer* a_buffer, condition* a_condition );
+            virtual ~record_receiver();
 
             void execute();
 
         private:
             server* f_server;
-            request_queue* f_request_queue;
+            buffer* f_buffer;
             condition* f_condition;
     };
 
