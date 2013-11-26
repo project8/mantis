8bc0a72d33542a6bf46b06a2f7eccefa79e316fd
Noah Oblath <nsoblath@mit.edu>
2013-11-26 15:39:11 -0500

    Adapted test_mantis_client to the new class names and re-added it to the build.

--- Applications/test_mantis_client.cc
+++ Applications/test_mantis_client.cc
@@ -1,6 +1,6 @@
 #include "mt_parser.hh"
 #include "mt_client.hh"
-#include "mt_run_context.hh"
+#include "mt_request_dist.hh"
 #include "thorax.hh"
 using namespace mantis;
 
@@ -14,9 +14,9 @@ using std::stringstream;
 using std::cout;
 using std::endl;
 
-int analyze_status( run_context* t_run_context )
+int analyze_status( request_dist* t_request_dist )
 {
-    switch( t_run_context->get_status()->state() )
+    switch( t_request_dist->get_status()->state() )
     {
         case status_state_t_acknowledged :
             cout << "[test_mantis_client] acknowledged..." << '\r';
@@ -57,33 +57,36 @@ int main( int argc, char** argv )
     parser t_parser( argc, argv );
 
     client* t_client = new client( t_parser.get_required< string >( "host" ), t_parser.get_required< int >( "port" ) );
-    run_context* t_run_context = new run_context();
-    t_run_context->set_connection( t_client );
+    request_dist* t_request_dist = new request_dist();
+    t_request_dist->set_connection( t_client );
 
-    t_run_context->get_request()->set_file( "/data/ohgod.egg" );
-    t_run_context->get_request()->set_description( "junk" );
-    t_run_context->get_request()->set_date( get_absolute_time_string() );
-    t_run_context->get_request()->set_mode( request_mode_t_single );
-    t_run_context->get_request()->set_rate( 800.0 );
-    t_run_context->get_request()->set_duration( 2000.0 );
+    t_request_dist->get_request()->set_write_host( "" );
+    t_request_dist->get_request()->set_write_port( -1 );
+    t_request_dist->get_request()->set_file( "/data/ohgod.egg" );
+    t_request_dist->get_request()->set_description( "junk" );
+    t_request_dist->get_request()->set_date( get_absolute_time_string() );
+    t_request_dist->get_request()->set_mode( request_mode_t_single );
+    t_request_dist->get_request()->set_rate( 800.0 );
+    t_request_dist->get_request()->set_duration( 2000.0 );
+    t_request_dist->get_request()->set_file_write_mode( request_file_write_mode_t_local );
 
     cout << "[test_mantis_client] sending request..." << endl;
-    cout << t_run_context->get_request()->DebugString() << endl;
+    cout << t_request_dist->get_request()->DebugString() << endl;
 
-    t_run_context->push_request();
+    t_request_dist->push_request();
 
-    t_run_context->pull_status();
-    analyze_status( t_run_context );
+    t_request_dist->pull_status();
+    analyze_status( t_request_dist );
 
 //    do
 //    {
-//        t_run_context->pull_status();
+//        t_request_dist->pull_status();
 //    }
 //    while( analyze_status( t_run_context ) == 0 );
 
     cout << "[test_mantis_client] done" << endl;
 
-    delete t_run_context;
+    delete t_request_dist;
     delete t_client;
 
     return 0;
