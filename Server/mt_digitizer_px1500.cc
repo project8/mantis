1ad8816a4d3e29434780b098ef39560d3d48f14d
DJSecretSnaxxx <dj_secret_snacks@hyperion-targeting-instustries.com>
2014-05-05 16:01:47 -0700

    For the px1500 digitizer, moved acquiring the timestamp for each record to after the record record values are acquired to avoid the variable time it takes the px1500 to get the first record

--- Server/mt_digitizer_px1500.cc
+++ Server/mt_digitizer_px1500.cc
@@ -356,8 +356,6 @@ namespace mantis
     {
         a_block->set_record_id( f_record_count );
         a_block->set_acquisition_id( f_acquisition_count );
-        get_time_monotonic( &a_stamp_time );
-        a_block->set_timestamp( time_to_nsec( a_stamp_time ) - f_start_time );
 
         int t_result = GetPciAcquisitionDataFastPX4( f_handle, f_buffer->record_size(), a_block->data_bytes(), 0 );
         if( t_result != SIG_SUCCESS )
@@ -366,6 +364,10 @@ namespace mantis
             t_result = EndBufferedPciAcquisitionPX4( f_handle );
             return false;
         }
+
+        get_time_monotonic( &a_stamp_time );
+        a_block->set_timestamp( time_to_nsec( a_stamp_time ) - f_start_time );
+        MTWARN( mtlog, "acq time: " << a_block->get_timestamp() );
 
         ++f_record_count;
 
