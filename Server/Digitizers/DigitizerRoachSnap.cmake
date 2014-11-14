45366e14b86db42355cb1581f09f28b5464143f1
Noah Oblath <nsoblath@mit.edu>
2014-11-14 11:30:46 -0500

    Moved mt_digitzer_roach to Server/Digitizers. Added DigitizerRoachSnap.cmake.

---
+++ Server/Digitizers/DigitizerRoachSnap.cmake
@@ -0,0 +1,34 @@
+##########################################
+# Find libraries, include directories, etc
+##########################################
+if( NOT KATCP_FOUND )
+    add_subdirectory( katcp_devel )
+    pbuilder_add_ext_libraries( ${katcp_devel_LIBRARIES} dl )
+    # add to the RPATH to be used when installing, but only if it's not a system directory
+    list( FIND CMAKE_PLATFORM_IMPLICIT_LINK_DIRECTORIES "${katcp_devel_LIBRARIES}" isSystemDir )
+    if( "${isSystemDir}" STREQUAL "-1" )
+        list( APPEND CMAKE_INSTALL_RPATH ${katcp_devel_LIBRARIES} )
+    endif( "${isSystemDir}" STREQUAL "-1" )
+    include_directories( ${PROJECT_SOURCE_DIR}/katcp_devel/katcp )
+    set( KATCP_FOUND TRUE )
+endif( NOT KATCP_FOUND )
+
+
+#############################
+# Add header and source files
+#############################
+
+add_digitizer_header_files(
+    mt_digitzer_roach.hh
+)
+
+add_digitizer_source_files(
+    mt_digitizer_roach.cc
+)
+
+
+######
+# Done
+######
+
+message( STATUS "Including the RoachSnap digitizer" )
