366142d215790062d52abac297b49fb6af09d6a0
Noah Oblath <nsoblath@mit.edu>
2014-11-17 15:49:51 -0500

    Re-added katcp_devel into Server/Devices. Fixed up the roach-snap build.

--- Server/Devices/DigitizerRoachSnap.cmake
+++ Server/Devices/DigitizerRoachSnap.cmake
@@ -2,14 +2,14 @@
 # Find libraries, include directories, etc
 ##########################################
 if( NOT KATCP_FOUND )
-    add_subdirectory( Digitizers/katcp_devel )
+    add_subdirectory( Devices/katcp_devel )
     pbuilder_add_ext_libraries( ${katcp_devel_LIBRARIES} dl )
     # add to the RPATH to be used when installing, but only if it's not a system directory
     list( FIND CMAKE_PLATFORM_IMPLICIT_LINK_DIRECTORIES "${katcp_devel_LIBRARIES}" isSystemDir )
     if( "${isSystemDir}" STREQUAL "-1" )
         list( APPEND CMAKE_INSTALL_RPATH ${katcp_devel_LIBRARIES} )
     endif( "${isSystemDir}" STREQUAL "-1" )
-    include_directories( Digitizers/katcp_devel/katcp )
+    include_directories( Devices/katcp_devel/katcp )
     set( KATCP_FOUND TRUE )
 endif( NOT KATCP_FOUND )
 
@@ -19,7 +19,7 @@ endif( NOT KATCP_FOUND )
 #############################
 
 add_digitizer_header_files(
-    mt_digitzer_roach_snap.hh
+    mt_digitizer_roach_snap.hh
 )
 
 add_digitizer_source_files(
