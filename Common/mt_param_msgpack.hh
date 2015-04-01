f96e947aff12c7613bcacc45a204ee333f035dd1
Ben LaRoque <benjamin.laroque@gmail.com>
2015-04-01 11:10:51 -0700

    fixing comment

--- Common/mt_param_msgpack.hh
+++ Common/mt_param_msgpack.hh
@@ -1,12 +1,12 @@
 /*
- * mt_param_json.hh
+ * mt_param_msgpack.hh
  *
  *  Created on: March 27, 2015
- *      Author: nsoblath, bhlaroque
+ *      Author: bhlaroque
  */
 
-#ifndef MT_PARAM_JSON_HH_
-#define MT_PARAM_JSON_HH_
+#ifndef MT_PARAM_MSGPACK_HH_
+#define MT_PARAM_MSGPACK_HH_
 
 #include "mt_exception.hh"
 
@@ -30,21 +30,21 @@
 
 namespace mantis
 {
-    MTLOGGER(mtlog_param_json, "param");
+    MTLOGGER(mtlog_param_msgpack, "param");
 
     //***************************************
     //************** INPUT ******************
     //***************************************
 
-    class MANTIS_API param_input_json
+    class MANTIS_API param_input_msgpack
     {
         public:
-            param_input_json();
-            virtual ~param_input_json();
+            param_input_msgpack();
+            virtual ~param_input_msgpack();
 
-            static param_node* read_file( const std::string& a_filename );
-            static param_node* read_string( const std::string& a_json_str );
-            static param_node* read_document( const rapidjson::Document& a_document );
+            //static param_node* read_file( const std::string& a_filename );
+            static param_node* read_string( const std::string& a_msgpack_str );
+            //static param_node* read_document( const rapidjson::Document& a_document );
             static param* read_value( const rapidjson::Value& a_value );
     };
 
@@ -52,7 +52,7 @@ namespace mantis
     //************** OUTPUT *****************
     //***************************************
 
-    class MANTIS_API param_output_json
+    class MANTIS_API param_output_msgpack
     {
         public:
             typedef rapidjson::Writer< rapidjson::FileStream, rapidjson::UTF8<>, rapidjson::MemoryPoolAllocator<> > rj_file_writer;
@@ -67,8 +67,8 @@ namespace mantis
             };
 
         public:
-            param_output_json();
-            virtual ~param_output_json();
+            param_output_msgpack();
+            virtual ~param_output_msgpack();
 
             static bool write_file( const param& a_to_write, const std::string& a_filename, json_writing_style a_style );
             static bool write_string( const param& a_to_write, std::string& a_string, json_writing_style a_style );
@@ -86,75 +86,51 @@ namespace mantis
     };
 
     template< class XWriter >
-    bool param_output_json::write_param( const param& a_to_write, XWriter* a_writer )
+    bool param_output_msgpack::write_param( const param& a_to_write, XWriter* a_writer )
     {
         if( a_to_write.is_null() )
         {
-            return param_output_json::write_param_null( a_to_write, a_writer );
+            return param_output_msgpack::write_param_null( a_to_write, a_writer );
         }
         if( a_to_write.is_value() )
         {
-            return param_output_json::write_param_value( a_to_write.as_value(), a_writer );
+            return param_output_msgpack::write_param_value( a_to_write.as_value(), a_writer );
         }
         if( a_to_write.is_array() )
         {
-            return param_output_json::write_param_array( a_to_write.as_array(), a_writer );
+            return param_output_msgpack::write_param_array( a_to_write.as_array(), a_writer );
         }
         if( a_to_write.is_node() )
         {
-            return param_output_json::write_param_node( a_to_write.as_node(), a_writer );
+            return param_output_msgpack::write_param_node( a_to_write.as_node(), a_writer );
         }
-        MTWARN( mtlog_param_json, "parameter not written: <" << a_to_write << ">" );
+        MTWARN( mtlog_param_msgpack, "parameter not written: <" << a_to_write << ">" );
         return false;
     }
     template< class XWriter >
-    bool param_output_json::write_param_null( const param& /*a_to_write*/, XWriter* a_writer )
+    bool param_output_msgpack::write_param_null( const param& /*a_to_write*/, XWriter* a_writer )
     {
-        //MTWARN( mtlog_param_json, "writing null" );
+        //MTWARN( mtlog_param_msgpack, "writing null" );
         a_writer->Null();
         return true;
     }
     template< class XWriter >
-    bool param_output_json::write_param_value( const param_value& a_to_write, XWriter* a_writer )
+    bool param_output_msgpack::write_param_value( const param_value& a_to_write, XWriter* a_writer )
     {
-        //MTWARN( mtlog_param_json, "writing value" );
-        if( a_to_write.is_string() )
-        {
-            a_writer->String( a_to_write.as_string().c_str() );
-            //MTWARN( mtlog_param_json, "writing string to json: " << a_to_write.as_string() );
-        }
-        else if( a_to_write.is_bool() )
-        {
-            a_writer->Bool( a_to_write.as_bool() );
-            //MTWARN( mtlog_param_json, "writing bool to json: " << a_to_write.as_bool() );
-        }
-        else if( a_to_write.is_int() )
-        {
-            a_writer->Int64( a_to_write.as_int() );
-            //MTWARN( mtlog_param_json, "writing int to json: " << a_to_write.as_int() );
-        }
-        else if( a_to_write.is_uint() )
-        {
-            a_writer->Uint64( a_to_write.as_uint() );
-            //MTWARN( mtlog_param_json, "writing uint to json: " << a_to_write.as_uint() );
-        }
-        else if( a_to_write.is_double() )
-        {
-            a_writer->Double( a_to_write.as_double() );
-            //MTWARN( mtlog_param_json, "writing double to json: " << a_to_write.as_double() );
-        }
+        //MTWARN( mtlog_param_msgpack, "writing value" );
+        a_writer->String(a_to_write.to_string().c_str());
         return true;
     }
     template< class XWriter >
-    bool param_output_json::write_param_array( const param_array& a_to_write, XWriter* a_writer )
+    bool param_output_msgpack::write_param_array( const param_array& a_to_write, XWriter* a_writer )
     {
-        //MTWARN( mtlog_param_json, "writing array" );
+        //MTWARN( mtlog_param_msgpack, "writing array" );
         a_writer->StartArray();
         for( param_array::const_iterator it = a_to_write.begin(); it != a_to_write.end(); ++it )
         {
-            if( ! param_output_json::write_param( *(*it), a_writer ) )
+            if( ! param_output_msgpack::write_param( *(*it), a_writer ) )
             {
-                MTERROR( mtlog_param_json, "Error while writing parameter array" );
+                MTERROR( mtlog_param_msgpack, "Error while writing parameter array" );
                 return false;
             }
         }
@@ -162,16 +138,16 @@ namespace mantis
         return true;
     }
     template< class XWriter >
-    bool param_output_json::write_param_node( const param_node& a_to_write, XWriter* a_writer )
+    bool param_output_msgpack::write_param_node( const param_node& a_to_write, XWriter* a_writer )
     {
-        //MTWARN( mtlog_param_json, "writing node" );
+        //MTWARN( mtlog_param_msgpack, "writing node" );
         a_writer->StartObject();
         for( param_node::const_iterator it = a_to_write.begin(); it != a_to_write.end(); ++it )
         {
             a_writer->String( it->first.c_str() );
-            if( ! param_output_json::write_param( *(it->second), a_writer ) )
+            if( ! param_output_msgpack::write_param( *(it->second), a_writer ) )
             {
-                MTERROR( mtlog_param_json, "Error while writing parameter node" );
+                MTERROR( mtlog_param_msgpack, "Error while writing parameter node" );
                 return false;
             }
         }
@@ -182,4 +158,4 @@ namespace mantis
 
 } /* namespace mantis */
 
-#endif /* MT_PARAM_JSON_HH_ */
+#endif /* MT_PARAM_HH_ */
