03d768d5b4f04ae497917397e39ef8f8c0eb98c5
Ben LaRoque <benjamin.laroque@gmail.com>
2015-04-14 17:34:37 -0700

    recursive msg parsing

--- Common/mt_param_msgpack.hh
+++ Common/mt_param_msgpack.hh
@@ -46,7 +46,7 @@ namespace mantis
 
             //static param_node* read_file( const std::string& a_filename );
             static param_node* read_string( const std::string& a_msgpack_str );
-            static param_node* read_msgpack_array( const msgpack::object_array& a_msgpack_array );
+            static param* read_msgpack_element( const msgpack::object& a_msgpack_array );
             //static param_node* read_document( const rapidjson::Document& a_document );
             //static param* read_value( const rapidjson::Value& a_value );
     };
